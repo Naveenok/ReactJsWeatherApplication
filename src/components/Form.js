fbddd2361e28e048bdfd9121fa1f3e3d57fc9ac0
oknaveen <oknaveen9395@gmail.com>
2019-09-15 09:46:49 -0400

    title and the form component set up

---
+++ src/components/Form.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Form(){
+    return(
+        <h1>hello form</h1>
+    );
+}
+export default Form;
---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Title(){
+    return(
+        <h1>I'm title</h1>
+
+    );
+}
+export default Title;
