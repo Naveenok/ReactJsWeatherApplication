e13d3931a374494c3cc63435f051db8f6e972d21
oknaveen <oknaveen9395@gmail.com>
2019-09-15 18:29:56 -0400

    changed the state according to the input and rendered the weather component

--- src/App.js
+++ src/App.js
@@ -8,20 +8,56 @@ import './App.css';
 const API_KEY = "a26bb5958fcdbfcc2ef72c49cdb6d134";
 
 class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+
+  }
+
   getWeather = async (e) => {
-    e.preventDefault();  
-    const city=e.target.elements.city.value;  
-    const country=e.target.elements.country.value;  
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
     const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
     const data = await api_call.json();
-    console.log(data);
+    if (city && country) {
+      console.log(data);
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    }
+    else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter the correct"
+      });
+    }
+
   }
   render() {
     return (
       <div>
         <Title />
         <Form weather={this.getWeather} />
-        <Weather />
+        <Weather temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error} />
 
       </div>
 
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,7 +3,15 @@ import React from 'react';
 class Weather extends React.Component {
     render() {
         return (
-            <h1>hello Weather</h1>
+            <div>{this.props.city && <p> temperature : {this.props.city}</p>}
+                {this.props.country && <p> country :  {this.props.country}</p>}
+                {this.props.temperature && <p> Temp : {this.props.temperature}</p>}
+                {this.props.humidity && <p>Humidity : {this.props.humidity}</p>}
+                {this.props.description && <p>description : {this.props.description}</p>}
+                {this.props.error && <p>{this.props.error}</p>}
+
+
+            </div>
         );
     }
 
